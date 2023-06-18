4492e1d0edd245a6119bc41bb98e43c307e5cf9f
amad3us47 <markus39107@gmail.com>
2023-06-18 19:08:48 +0530

    cool

--- 1/twosum.java
+++ 1/twosum.java
@@ -1,6 +1,22 @@
 import java.util.*;
 class twosum
 {
-  public static void main(String args[])
-  Scanner in = new Scanner(System.in)
+public static void main(String args[])
+{
+    Scanner in =new Scanner(System.in);
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    System.out.println("Enter the number");
+    int target=in.nextInt();
+    for(int i=0;i<arr.length;i++)     //looping through the elements in array
+    {
+      int find = target - arr[i];      
+      for(int j=0;j<arr.length;j++)    
+    {   
+        if(find==arr[j])              //finding the number in second loop after subracting the first index from the target
+      {
+          System.out.println(arr[i]+"  "+find);
+        }
+      }
+    }
+}
 }
